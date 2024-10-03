792212a37dbd9ea591384262f34cae5e2fd80b1d
Daan de Graaf <d.degraaf@dictu.nl>
2024-10-03 08:58:30 +0200

    Multi module project

---
+++ rest-workshop/server-codegen/src/main/java/nl/ddaaaaann/restworkshop/servercodegen/HotelServiceApplication.java
@@ -0,0 +1,12 @@
+package nl.ddaaaaann.restworkshop.servercodegen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(HotelServiceApplication.class, args);
+  }
+}
